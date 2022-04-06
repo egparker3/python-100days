3f7159b46bd4a131ef366e49d4433875b2d9f329
egparker3 <egparker3@gmail.com>
2022-04-06 15:56:03 -0700

    day 1 of lesson

---
+++ Day 1/day1.py
@@ -0,0 +1,17 @@
+#Today is day 1 of my 100 Days of Code: The Complete Python Pro Bootcamp course. April 6th 2022
+print("hello world")
+# Write your code below this line 👇
+print("Hello world!")
+#print function follows parentheses, inside the parentheses you tell it what you want to print.
+#in order to tell computer that something is not code, you need to use quotes
+#Strings: a line of characters, with the quotes showing beginning and end of the string of characters
+#if you don't have the ending quotes, the ending parenthesis will not be the same color as the beginning parenthesis 
+#if you come across a red error, copy and paste the error into google, with stackoverflow.com usually having an answer
+#OK, the first assignement she gave us is through Coding Rooms, and the input is the following:
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+#I got the answer correct on the first try!
+#using double quotes or single quotes doesn't matter for printing
+print('hello')
+#however, if you want to have a quote inside a quote, you need to use the single quote inside the double quote, so that the computer knows where your strings are. If you used a double quote then another double quote, it would think your string is the print and the function/code comes after the first double quote. 
