e2529a174924dab6576c13f9116bca8e8742167c
egparker3 <egparker3@gmail.com>
2022-04-17 19:11:09 -0700

    day2

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -167,3 +167,25 @@ isWinning = True
 
 print(f"your score is {score}, your height is{height}, you are winning is {isWinning}")
 #f-strings turn everything into a string
+
+# 4/17/2022
+
+#this was a project for how much life you have left:
+
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+age_int = int(age)
+
+years_left = (90 - age_int)
+
+months = years_left * 12
+
+weeks = years_left * 52
+
+days = years_left * 365
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
